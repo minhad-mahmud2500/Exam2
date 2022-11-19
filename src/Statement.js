952e9391af5531bcdac7dd6e6971cb82b53579bb
minhad-mahmud2500 <87450136+minhad-mahmud2500@users.noreply.github.com>
2022-11-19 11:26:56 -0500

    Update Statement.js

--- src/Statement.js
+++ src/Statement.js
@@ -1,10 +1,11 @@
 import stack from "./stack";
-import DataTypes from "./misc/DataTypes";
-import Assign from "./statements/Assign";
-import Tokens from "./misc/Tokens";
-import Declaration from "./statements/Declaration";
-import Condition from "./statements/Condition";
-import Loop from "./statements/Loop";
+import { DataTypes } from "./misc/DataTypes";
+import { Assign } from "./statements/Assign";
+import { Tokens } from "./misc/Tokens";
+import { Declaration } from "./statements/Declaration";
+import { Condition } from "./statements/Condition";
+import { Loop } from "./statements/Loop";
+import { stack } from "./stack";
 export default function Statement(statement = "") {
     statement = statement.trim();
     if (!statement)
@@ -18,19 +19,15 @@ export default function Statement(statement = "") {
         Declaration(statement);
     }
     else if (firstWord == Tokens.REPEAT) {
-        // while statement
         Loop(statement);
     }
     else if (firstWord == Tokens.COND) {
-        // if statement
         Condition(statement);
     }
     else if (stack[firstWord] != undefined) {
         Assign(statement);
-        // is an assigny operator
     }
     else {
-        // throw error
         throw new Error(`${firstWord} isn't defined`);
     }
 }
