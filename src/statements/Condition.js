60235a73df4ca187193ac9e5f557d6cb1abc8105
minhad-mahmud2500 <87450136+minhad-mahmud2500@users.noreply.github.com>
2022-11-19 11:42:07 -0500

    Update Condition.js

--- src/statements/Condition.js
+++ src/statements/Condition.js
@@ -1,7 +1,7 @@
-import BooleanEquation from "../misc/BooleanEquation";
-import InsideBrackets from "../misc/InsideBrackets";
-import Tokens from "../misc/Tokens";
-import Statement from "../Statement";
+import { BooleanEquation } from "../misc/BooleanEquation";
+import { InsideBracket }  from "../misc/InsideBrackets";
+import { Tokens } from "../misc/Tokens";
+import { Statement } from "../Statement";
 export default function Condition(statement) {
     statement = statement.replace("cond ", "");
     const { insideStatement: booleanEquation, restStatement: conditionStatement, } = InsideBrackets(statement, Tokens.PARENTHESIS_OPEN, Tokens.PARENTHESIS_CLOSE);
