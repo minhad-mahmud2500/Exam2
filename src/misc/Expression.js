c7e63ff3c2d29dbcb71aec41eea40a118fe1d19c
minhad-mahmud2500 <87450136+minhad-mahmud2500@users.noreply.github.com>
2022-11-19 11:44:16 -0500

    Update Expression.js

--- src/misc/Expression.js
+++ src/misc/Expression.js
@@ -1,4 +1,4 @@
-import LexicalAnalyzer from "./LexicalAnalyzer";
+import { LexicalAnalyzer } from "./LexicalAnalyzer";
 export default function Expression(expression) {
     const lexicalAnalyzer = new LexicalAnalyzer(expression.trim());
     return lexicalAnalyzer.getEquation();
