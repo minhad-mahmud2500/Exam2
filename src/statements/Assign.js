be0b85e57030aaf311f5741cb3d59a9ee4d3ad91
minhad-mahmud2500 <87450136+minhad-mahmud2500@users.noreply.github.com>
2022-11-19 11:05:33 -0500

    Create Assign.js

---
+++ src/statements/Assign.js
@@ -0,0 +1,21 @@
+import stack from "../stack";
+import Tokens from "../misc/Tokens";
+import Statement from "../Statement";
+import Expression from "../misc/Expression";
+
+export default function Assign(statement: string) {
+  const regexMatch = statement.match(/([^;]+);(.*)/)!;
+
+  if (!regexMatch) throw new Error("Invalid formatting while assigning");
+
+  const [_, assignStatement, restStatement] = regexMatch;
+
+  let [variable, expression] = assignStatement.split(Tokens.ASSIGN);
+
+  variable = variable.trim();
+  expression = expression.trim();
+
+  stack[variable][1] = Expression(expression);
+
+  Statement(restStatement.trim());
+}
