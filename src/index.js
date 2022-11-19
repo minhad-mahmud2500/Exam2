f3ec4b8c9b993f1f1fb596ff2932ae659abddf89
minhad-mahmud2500 <87450136+minhad-mahmud2500@users.noreply.github.com>
2022-11-19 11:19:29 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import fs from "fs/promises";
+import path from "path";
+import stack from "./stack";
+import Statement from "./Statement";
+const main = async () => {
+    const filePath = path.resolve(process.cwd(), process.argv[2]);
+    // Gets all the statements between begin and end
+    const exceCode = (await fs.readFile(filePath, "ascii"))
+        .trim()
+        .replaceAll("\n", " ")
+        .replaceAll(/\s+/g, " ");
+    const result = [...exceCode.matchAll(/begin (.*) end/gm)];
+    const statement = result[0] ? result[0][1] : "";
+    Statement(statement);
+};
+main()
+    .then(() => console.log(stack))
+    .catch((err) => console.log("Error:", err));
